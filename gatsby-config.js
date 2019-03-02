1258121eef043ed45d8687b8f2e4a6dc10c3e590
Gilbert Lucas <gilbertlucas46@gmail.com>
2019-03-02 23:00:59 +0800

    updated manifest information

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: { 
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Gilbert Lucas`,
+        short_name: `gilbert`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
