4c819bdd32bfdac73566ab08956fee03f07cee9e
Gilbert <gilbertlucas46@gmail.com>
2019-03-16 22:24:16 +0800

    added slider

--- src/components/services.js
+++ src/components/services.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react'
 import { StaticQuery, graphql, Link } from "gatsby"
 import styled from 'styled-components'
-import Layout from './layout';
+import Img from 'gatsby-image'
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 
 const SERVICES_QUERY = graphql`
-  query Services {
-    allMarkdownRemark(filter: {frontmatter: {categories: {eq: "services"}}}) {
+   query Services {
+    allMarkdownRemark(filter: {frontmatter: {categories: {eq: "services"}}},sort: {
+    order: DESC,
+    fields:frontmatter___thumbnail
+  }) {
             edges {
                 node {
                     frontmatter {
                         categories
                         title
+                         thumbnail {
+                           childImageSharp {
+                            fluid(maxWidth: 680) {
+                              ...GatsbyImageSharpFluid_withWebp_tracedSVG
+                            }
+                          }
+                      }
                     }
                 }
             }
@@ -19,35 +33,85 @@ const SERVICES_QUERY = graphql`
     }
 `;
 
-const ServiceList = styled.ul`
-  padding:0;
-  margin:0;
-  list-style:none; 
-  a {
-    font-size: 0.8rem;
-    text-decoration:underline;
-    color: #524763;
+const ServiceList = styled.div`
+  max-width:680px;
+  .slick-next{
+    right:20px;
+    z-index:99;
+  }
+  .slick-dots {
+   bottom: -35px;
+   text-align:left;
+  }
+  .slick-active{
+    button{
+      &:before {
+        color:#BCB2B2 !important;
+      }
+    }
+  }
+  .slick-prev {
+    left: 20px;
+    z-index:99;
+  }
+  .slick-dots {
+    li {
+      button {
+        &:before {
+          font-size:18px;
+        }
+      }
+    }
+  }
+  .slide {
+    position:relative;
+    margin-bottom:3rem;
+    h3 {
+      position: absolute;
+      z-index:99;
+      height: 3rem;
+      background-color: #292929;
+      text-align:center;
+      width:100%;
+      bottom:-2.9rem;
+      margin:0;
+      line-height:3rem;
+      font-size:1rem;
+      letter-spacing:1px;
+    }
   }
 `;
 
-const ServicesComponent = () => (
-    <StaticQuery
+export default class ServicesComponent extends Component {
+  render() {
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    };
+    return (
+      <StaticQuery
       query={SERVICES_QUERY}
       render={({allMarkdownRemark}) => (
-        <>
-          <div>
-              <ServiceList>
-              {allMarkdownRemark.edges.map(edge => (
-                  <li key={edge.node.frontmatter.title}>
-                    {edge.node.frontmatter.title}
-                  </li>
+          <ServiceList>
+            <Slider {...settings} >
+            {allMarkdownRemark.edges.map(edge => (
+              <div key={edge.node.frontmatter.title}>
+                <div className="slide">
+                <h3>{edge.node.frontmatter.title}</h3>
+                <Img fluid={edge.node.frontmatter.thumbnail.childImageSharp.fluid} />
+                </div>
+              </div>
               ))}
-              </ServiceList>
-          </div>
-        </>
+            </Slider>
+          
+          </ServiceList>
       )}
     />
-  )
+    )
+  }
+}
   
-  export default ServicesComponent
   
