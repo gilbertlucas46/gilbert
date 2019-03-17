bd46e6ae3ee5077f026046054e464e5d5f790aeb
Gilbert <gilbertlucas46@gmail.com>
2019-03-18 00:20:09 +0800

    form test 6

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,6 +3,147 @@ import Layout from  '../components/layout';
 import styled from 'styled-components'
 import Logo from '../components/logo'
 
+import { withFormik } from "formik";
+import * as Yup from "yup";
+import classnames from "classnames";
+import "../components/utils/form.css";
+
+const formikEnhancer = withFormik({
+    validationSchema: Yup.object().shape({
+      firstName: Yup.string()
+        .min(2, "C'mon, your name is longer than that")
+        .required("First name is required."),
+      lastName: Yup.string()
+        .min(2, "C'mon, your name is longer than that")
+        .required("Last name is required."),
+      email: Yup.string()
+        .email("Invalid email address")
+        .required("Email is required!")
+    }),
+  
+    mapPropsToValues: ({ user }) => ({
+      ...user
+    }),
+    handleSubmit: (payload, { setSubmitting }) => {
+      alert(payload.email);
+      setSubmitting(false);
+    },
+    displayName: "MyForm"
+  });
+
+
+const InputFeedback = ({ error }) =>
+error ? <div className="input-feedback">{error}</div> : null;
+
+const Label = ({ error, className, children, ...props }) => {
+  return (
+    <label className="label" {...props}>
+      {children}
+    </label>
+  );
+};
+
+const TextInput = ({
+    type,
+    id,
+    label,
+    error,
+    value,
+    onChange,
+    className,
+    ...props
+  }) => {
+    const classes = classnames(
+      "input-group",
+      {
+        "animated shake error": !!error
+      },
+      className
+    );
+    return (
+      <div className={classes}>
+        <Label htmlFor={id} error={error}>
+          {label}
+        </Label>
+        <input
+          id={id}
+          className="text-input"
+          type={type}
+          value={value}
+          onChange={onChange}
+          {...props}
+        />
+        <InputFeedback error={error} />
+      </div>
+    );
+  };
+
+  const MyForm = props => {
+    const {
+      values,
+      touched,
+      errors,
+      dirty,
+      handleChange,
+      handleBlur,
+      handleSubmit,
+      handleReset,
+      isSubmitting
+    } = props;
+    return (
+      <form
+        onSubmit={handleSubmit}
+        name="contact"
+        method="POST"
+        data-netlify-recaptcha="true"
+        data-netlify="true"
+      >
+        <TextInput
+          id="firstName"
+          type="text"
+          label="First Name"
+          placeholder="John"
+          error={touched.firstName && errors.firstName}
+          value={values.firstName}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
+        <TextInput
+          id="lastName"
+          type="text"
+          label="Last Name"
+          placeholder="Doe"
+          error={touched.lastName && errors.lastName}
+          value={values.lastName}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
+        <TextInput
+          id="email"
+          type="email"
+          label="Email"
+          placeholder="Enter your email"
+          error={touched.email && errors.email}
+          value={values.email}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
+        <button
+          type="button"
+          className="outline"
+          onClick={handleReset}
+          disabled={!dirty || isSubmitting}
+        >
+          Reset
+        </button>
+        <button type="submit" disabled={isSubmitting}>
+          Submit
+        </button>
+      </form>
+    );
+  };
+  
+  const MyEnhancedForm = formikEnhancer(MyForm);
 const ContactContent = styled.div`
     max-width:772px;
     height: 95vh;
@@ -33,16 +174,7 @@ const Contact = ({location}) => (
             <h1>Contact Me</h1>
             <p>Let’s build something together! Or <a href="mailto:lucas@gilbert.codes?Subject=Hi Gilbert!" target="_top">email</a> email me if you have
             any other questions.</p>
-            <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" action="/thankyou">
-            {/* You still need to add the hidden input with the form name to your JSX form */}
-            <input type="hidden" name="form-name" value="contact" />
-            <p>
-                <label>name: <input type="text" name="name" /></label>
-            </p>
-            <p>
-                <button>Send</button>
-            </p>
-            </form>
+            <MyEnhancedForm user={{ email: "", firstName: "", lastName: "" }} />
             <Logo/>
           </div>
         </ContactContent>
