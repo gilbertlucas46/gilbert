bc9d20a9ada12c57210be2327c7d18a46acc51d1
Gilbert <gilbertlucas46@gmail.com>
2019-03-31 08:51:23 +0800

    updated :cool: scrable text effect :fire:

--- src/components/utils/scrable.js
+++ src/components/utils/scrable.js
@@ -1,16 +1,99 @@
 import React, { Component } from 'react'
-import { Cycler } from "react-text-scrambler";
 
 export default class Scrable extends Component {
+  constructor(el) {
+    super(el)
+    this.el = el
+    this.chars = "!<>-_\\/[]{}—=+*^?#________"
+    // this.update = this.update.bind(this)
+  }
+  componentDidMount(){
+    const phrases = ["Freelancer", "Web Designer", "Fullstack Developer"]
+    
+    const el = document.querySelector('.text')
+    const fx = new TextScramble(el)
+    
+    let counter = 0
+    const next = () => {
+      fx.setText(phrases[counter]).then(() => {
+        setTimeout(next, 1000)
+      })
+      counter = (counter + 1) % phrases.length
+    }
+    
+    next()
+    console.log(el)
+  }
   render() {
-    const strings = ["Freelancer", "Web Designer", "Fullstack Developer"];
-    const characters = "!<>-{}/[]?=";
+    const phrases = [
+      "Neo,",
+      "sooner or later",
+      "you're going to realize",
+      "just as I did",
+      "that there's a difference",
+      "between knowing the path",
+      "and walking the path",
+    ]
+    
     return (
-        <Cycler
-        renderIn={1000}
-        duration={ 4000 }
-        characters={ characters }
-        strings={ strings } />
+      <>
+        <span className="text"></span>
+      </>
     )
+  }
+}
+
+class TextScramble {
+  constructor(el) {
+    this.el = el
+    this.chars = '!<>-_\\/[]{}—=+*^?#________'
+    this.update = this.update.bind(this)
+    console.log(this)
+  }
+  setText(newText) {
+    const oldText = this.el.innerText
+    const length = Math.max(oldText.length, newText.length)
+    const promise = new Promise((resolve) => this.resolve = resolve)
+    this.queue = []
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || ''
+      const to = newText[i] || ''
+      const start = Math.floor(Math.random() * 40)
+      const end = start + Math.floor(Math.random() * 40)
+      this.queue.push({ from, to, start, end })
+    }
+    cancelAnimationFrame(this.frameRequest)
+    this.frame = 0
+    this.update()
+    return promise
+  }
+  update() {
+    let output = ''
+    let complete = 0
+    for (let i = 0, n = this.queue.length; i < n; i++) {
+      let { from, to, start, end, char } = this.queue[i]
+      if (this.frame >= end) {
+        complete++
+        output += to
+      } else if (this.frame >= start) {
+        if (!char || Math.random() < 0.28) {
+          char = this.randomChar()
+          this.queue[i].char = char
+        }
+        output += `<span class="dud">${char}</span>`
+      } else {
+        output += from
+      }
+    }
+    this.el.innerHTML = output
+    if (complete === this.queue.length) {
+      this.resolve()
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update)
+      this.frame++
+    }
+  }
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)]
   }
 }
